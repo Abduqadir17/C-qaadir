d89bc4cb2a5a82ca102dab43ef1435a55bd4cdb9
Abduqadir17 <146451420+Abduqadir17@users.noreply.github.com>
2024-01-29 08:35:07 +0300

    Exercise seven.py

--- Exercise seven.py
+++ Exercise seven.py
@@ -1,5 +1,24 @@
-#QUESTION 7-2
-mex = ("1+3*6")
-result = eval("1+3*6")
-print(input("Answer is:"))
-print(result)
+#QUESTION 7-1
+def test_square_root(a,x):
+    while True:
+        print(x)
+        y=(x+ a/x)/2
+        if y==x:
+            break
+        x=y
+print(test_square_root(4,3))
+       
+
+
+#QUESTION 7-2  
+def eval_loop():
+    while True:
+        user_input = input("Enter a python exprssion(or 'done' to finish):")
+        if user_input =="done":
+            break
+        else:
+            result =eval(user_input)
+            print("Result:", result)
+
+#QUESTION 7-3
+            
